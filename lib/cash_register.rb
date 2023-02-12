c1521bbebab22b80d8126933d9a992fbf3512e85
+ <+>
2023-02-12 21:47:20 +0300

    commit tree 37a31dd2510474a23d43bbf233d90e171af921bb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,39 @@
 class CashRegister
 
-  attr_accessor :discount, :total, :add_item, :items, :last_transaction
+  attr_accessor :discount, :total
 
-  def initialize(discount = 0)
+  def initialize(discount = 20)
     @discount=discount
     @total=0
-    @items=[]
+  
   end
 
   def total=(total)
     @total=total
   end
-  def add_item(title,price,quantity=1)
-    @last_transaction = price*quantity
-    self.total += last_transaction
-    @items += [title]*quantity
-    
-  end 
-  def apply_discount
-    if discount != 0
-
-        @total = @total - @total * discount / 100
-        return "After the discount, the total comes to $#{@total}."
-
-      else
-        "There is no discount to apply."
-      end
-  end
-#   def items
+#   def add_item(title,price,quantity=1)
+#     @last_transaction = price*quantity
+#     self.total += last_transaction
 #     @items += [title]*quantity
     
+#   end 
+#   def apply_discount
+#     if discount != 0
+
+#         @total = @total - @total * discount / 100
+#         return "After the discount, the total comes to $#{@total}."
+
+#       else
+#         "There is no discount to apply."
+#       end
 #   end
-  def void_last_transaction
-    self.total -= last_transaction
-  end
+# #   def items
+# #     @items += [title]*quantity
+    
+# #   end
+#   def void_last_transaction
+#     self.total -= last_transaction
+#   end
 end
 #  cost=CashRegister.new()
 #  cost.add_item("eggs",10.0,6)
